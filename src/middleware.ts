e0f055b2774c0ea550ae5ecf741bc6d8ee7523c0
lucas paiva <lcspaiva87@gmail.com>
2023-11-05 01:23:50 -0300

    Remove unnecessary code in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,13 +12,12 @@ export default function middleware(request: NextRequest) {
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
-  
+
 }
 
 export const config = {
   matcher: [
     '/',
 		'/dashboard',
-    "/register",
   ]
 }
