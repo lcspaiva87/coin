cb79324a7bcffbc1c75e7c56b08bc98d52968bd7
lucas paiva <lcspaiva87@gmail.com>
2023-11-07 18:03:44 -0300

    Refactor header component to use SlideOvers instead of SlideOver

--- src/app/dashboard/components/SlideOver.tsx
+++ src/app/dashboard/components/SlideOver.tsx
@@ -1,22 +1,104 @@
-import { Dialog, Transition } from '@headlessui/react'
-import clsx from 'clsx'
-import { Fragment } from 'react'
+import { Icons } from "@/components/ui/icons";
+import { Dialog, Transition } from "@headlessui/react";
+import clsx from "clsx";
+import Link from "next/link";
+import { Fragment, useRef, useState } from "react";
+const SlideOvers = () => {
+  const [isSlideOverOpen, setIsSlideOverOpen] = useState(false);
+  const headerRef = useRef<HTMLElement>(null)
+  return (
+    <div>
+      <button
+        className="place-self-start lg:hidden"
+        onClick={() => setIsSlideOverOpen((prev) => !prev)}
+        aria-label="Open navigation menu"
+      >
+        <Icons.MenuHamburger />
+      </button>
+      <SlideOver
+        isOpen={isSlideOverOpen}
+        onClose={() => setIsSlideOverOpen(false)}
+        side="left"
+        hiddenBreakpoint="lg"
+        overlayClassName="top-[--top-distance]"
+        overlayStyle={{
+          "--top-distance": `${headerRef.current?.clientHeight ?? 0}px`,
+        }}
+        dialogClassName="mt-[--header-height] border-t border-secondary-300"
+        dialogStyle={{
+          "--header-height": `${headerRef.current?.clientHeight ?? 0}px`,
+        }}
+      >
+        <nav className="flex flex-col gap-8 px-6 py-4 ">
+          <SlideOverLink
+            href="#"
+            icon={(props) => <Icons.CryptoWallet {...props} />}
+          >
+            Lorem ipsum
+          </SlideOverLink>
+          <SlideOverLink
+            href="#"
+            icon={(props) => <Icons.CryptoCurrencyCircle {...props} />}
+          >
+            Lorem ipsum
+          </SlideOverLink>
+          <SlideOverLink
+            href="#"
+            icon={(props) => <Icons.CryptoCurrency {...props} />}
+          >
+            Lorem ipsum
+          </SlideOverLink>
+          <SlideOverLink href="#" icon={(props) => <Icons.Chart {...props} />}>
+            Lorem ipsum
+          </SlideOverLink>
 
-type Side = 'left' | 'right'
+          <button
+            aria-label="Close navigation menu"
+            className="mt-[14px] w-max"
+            onClick={() => setIsSlideOverOpen(false)}
+          >
+            <Icons.CircledArrowLeft className="h-6 w-6" />
+          </button>
+        </nav>
+      </SlideOver>
+    </div>
+  );
+};
+const SlideOverLink = ({
+  href,
+  icon,
+  children,
+}: {
+  href: string;
+  icon: (props: { className: string }) => React.ReactNode;
+  children: React.ReactNode;
+  onClick?: () => void;
+}) => {
+  return (
+    <Link
+      href={href}
+      className="flex items-center gap-4 text-label transition-colors hover:text-primary-400 hover:underline mt-[0.5rem] "
+    >
+      {icon({ className: "h-6 w-6" })}
+      {children}
+    </Link>
+  );
+};
+type Side = "left" | "right";
 
 type Props = {
-  side: Side
-  children: React.ReactNode
-  isOpen: boolean
-  onClose: () => void
-  overlayClassName?: string
-  overlayStyle?: any
-  dialogClassName?: string
-  dialogStyle?: any
-  hiddenBreakpoint?: 'md' | 'lg'
-}
+  side: Side;
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  overlayClassName?: string;
+  overlayStyle?: any;
+  dialogClassName?: string;
+  dialogStyle?: any;
+  hiddenBreakpoint?: "md" | "lg";
+};
 
-const  SlideOver = ({
+const SlideOver = ({
   side,
   children,
   isOpen,
@@ -25,16 +107,16 @@ const  SlideOver = ({
   overlayStyle,
   dialogClassName,
   dialogStyle,
-  hiddenBreakpoint = 'md',
+  hiddenBreakpoint = "md",
 }: Props) => {
   return (
     <div className="isolate">
       <Transition.Root show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className={clsx('relative z-10', {
-            'md:hidden': hiddenBreakpoint === 'md',
-            'lg:hidden': hiddenBreakpoint === 'lg',
+          className={clsx("relative z-10", {
+            "md:hidden": hiddenBreakpoint === "md",
+            "lg:hidden": hiddenBreakpoint === "lg",
           })}
           onClose={onClose}
         >
@@ -49,7 +131,7 @@ const  SlideOver = ({
           >
             <div
               className={clsx(
-                'fixed inset-0 bg-secondary-500 bg-opacity-75 transition-opacity',
+                "fixed inset-0 bg-secondary-500 bg-opacity-75 transition-opacity",
                 overlayClassName,
               )}
               style={overlayStyle}
@@ -60,26 +142,26 @@ const  SlideOver = ({
             <div className="absolute inset-0 overflow-hidden">
               <div
                 className={clsx(
-                  'pointer-events-none fixed inset-y-0 flex max-w-full',
-                  { 'left-0': side === 'left', 'right-0': side === 'right' },
+                  "pointer-events-none fixed inset-y-0 flex max-w-full",
+                  { "left-0": side === "left", "right-0": side === "right" },
                 )}
               >
                 <Transition.Child
                   as={Fragment}
                   enter="transform transition ease-in-out duration-500 sm:duration-700"
                   enterFrom={
-                    side === 'left' ? '-translate-x-full' : 'translate-x-full'
+                    side === "left" ? "-translate-x-full" : "translate-x-full"
                   }
                   enterTo="translate-x-0"
                   leave="transform transition ease-in-out duration-500 sm:duration-700"
                   leaveFrom="translate-x-0"
                   leaveTo={
-                    side === 'left' ? '-translate-x-full' : 'translate-x-full'
+                    side === "left" ? "-translate-x-full" : "translate-x-full"
                   }
                 >
                   <Dialog.Panel
                     className={clsx(
-                      'pointer-events-auto relative',
+                      "pointer-events-auto relative",
                       dialogClassName,
                     )}
                     style={dialogStyle}
@@ -95,7 +177,7 @@ const  SlideOver = ({
         </Dialog>
       </Transition.Root>
     </div>
-  )
-}
+  );
+};
 
-export default SlideOver
+export default SlideOvers;
--- src/app/dashboard/components/header.tsx
+++ src/app/dashboard/components/header.tsx
@@ -1,32 +1,22 @@
 "use client";
-import { Icons } from "@/components/ui/icons";
 
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import Link from "next/link";
-import { useRef, useState } from "react";
-import SlideOver from "./SlideOver";
-
+import SlideOvers from "./SlideOver";
 
 export default function Header() {
-  const [isSlideOverOpen, setIsSlideOverOpen] = useState(false);
-  const DropdownMenu = dynamic(() => import('./dropdownMenu'), {
+
+  const DropdownMenu = dynamic(() => import("./dropdownMenu"), {
     ssr: false,
-  })
-  const headerRef = useRef<HTMLElement>(null);
+  });
+
   return (
     <>
       <header
         className="z-10 shadow-[0px_4px_8px_rgba(77,77,77,0.1)] grid grid-cols-3 grid-rows-1 py-[1rem] lg:flex lg:items-center lg:justify-between h-[4rem] px-[2.5rem]"
-        ref={headerRef}
+
       >
-        <button
-          className="place-self-start lg:hidden"
-          onClick={() => setIsSlideOverOpen((prev) => !prev)}
-          aria-label="Open navigation menu"
-        >
-          <Icons.MenuHamburger />
-        </button>
+        <SlideOvers />
         <Image
           src="./logo.svg"
           className="place-self-center"
@@ -36,73 +26,6 @@ export default function Header() {
         />
         <DropdownMenu className="place-self-end" />
       </header>
-
-      <SlideOver
-        isOpen={isSlideOverOpen}
-        onClose={() => setIsSlideOverOpen(false)}
-        side="left"
-        hiddenBreakpoint="lg"
-        overlayClassName="top-[--top-distance]"
-        overlayStyle={{
-          '--top-distance': `${headerRef.current?.clientHeight ?? 0}px`,
-        }}
-        dialogClassName="mt-[--header-height] border-t border-secondary-300"
-        dialogStyle={{
-          '--header-height': `${headerRef.current?.clientHeight ?? 0}px`,
-        }}
-      >
-        <nav className="flex flex-col gap-8 px-6 py-4 ">
-          <SlideOverLink
-            href="#"
-            icon={(props) => <Icons.CryptoWallet {...props} />}
-          >
-            Lorem ipsum
-          </SlideOverLink>
-          <SlideOverLink
-            href="#"
-            icon={(props) => <Icons.CryptoCurrencyCircle {...props} />}
-          >
-            Lorem ipsum
-          </SlideOverLink>
-          <SlideOverLink
-            href="#"
-            icon={(props) => <Icons.CryptoCurrency {...props} />}
-          >
-            Lorem ipsum
-          </SlideOverLink>
-          <SlideOverLink href="#" icon={(props) => <Icons.Chart {...props} />}>
-            Lorem ipsum
-          </SlideOverLink>
-
-          <button
-            aria-label="Close navigation menu"
-            className="mt-[14px] w-max"
-            onClick={() => setIsSlideOverOpen(false)}
-          >
-            <Icons.CircledArrowLeft className="h-6 w-6" />
-          </button>
-        </nav>
-      </SlideOver>
     </>
   );
 }
-const SlideOverLink = ({
-  href,
-  icon,
-  children,
-}: {
-  href: string;
-  icon: (props: { className: string }) => React.ReactNode;
-  children: React.ReactNode;
-  onClick?: () => void;
-}) => {
-  return (
-    <Link
-      href={href}
-      className="flex items-center gap-4 text-label transition-colors hover:text-primary hover:underline"
-    >
-      {icon({ className: "h-6 w-6" })}
-      {children}
-    </Link>
-  );
-};
