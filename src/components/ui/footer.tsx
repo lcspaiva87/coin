1b41b48dfed741bb90dcbfddc414c724abfbc14d
lucas paiva <lcspaiva87@gmail.com>
2023-11-08 18:02:08 -0300

    Update footer styles and content

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,8 +3,8 @@ import Logo from "/public/logo.svg";
 
 export const Footer = () => {
   return (
-    <footer className="z-10 shadow-[0px_-4px_8px_rgba(77,77,77,0.1)]">
-      <div className="container flex items-center justify-center py-5 md:justify-between lg:py-6">
+    <footer className=" border-t-4  border-secondary-800/10  ">
+        <div className=" justify-between flex items-center px-[1.5rem]   py-5 md:justify-between lg:py-6">
         <p className="hidden text-label md:block">
           Copyright © 2023 - All rights reserved
         </p>
